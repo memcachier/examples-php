88eacd2bc926efc25ed4f65be17ef33623ecf821
David Terei <code@davidterei.com>
2014-04-22 10:43:15 -0700

    Restructure source code.

--- index.php
+++ index.php
@@ -2,81 +2,32 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>PHP caching example</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="MemCachier PHP Example">
     <meta name="author" content="MemCachier">
 
-    <link href="/bootstrap.min.css" rel="stylesheet" media="screen"/>
-    <style type="text/css">
-      /* Sticky footer styles
-      -------------------------------------------------- */
-      html,
-      body {
-        height: 100%;
-        /* The html and body elements cannot have any padding or margin. */
-      }
+    <title>PHP caching example</title>
 
-      /* Wrapper for page content to push down footer */
-      #wrap {
-        min-height: 100%;
-        height: auto !important;
-        height: 100%;
-        /* Negative indent footer by it's height */
-        margin: 0 auto -60px;
-      }
-
-      /* Set the fixed height of the footer here */
-      #push,
-      #footer {
-        height: 60px;
-      }
-      #footer {
-        background-color: #f5f5f5;
-      }
-
-      /* Lastly, apply responsive CSS fixes as necessary */
-      @media (max-width: 767px) {
-        #footer {
-          margin-left: -20px;
-          margin-right: -20px;
-          padding-left: 20px;
-          padding-right: 20px;
-        }
-      }
-
-      /* Custom page CSS
-      -------------------------------------------------- */
-      /* Not required for template or sticky footer method. */
-      .container {
-        width: auto;
-        max-width: 680px;
-      }
-      .container .credit {
-        margin: 20px 0;
-      }
-    </style>
-    <link href="/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+    <link href="/public/bootstrap.min.css" rel="stylesheet" media="screen"/>
+    <link href="/public/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+    <link href="/public/app.css" rel="stylesheet" media="screen"/>
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="/html5shiv.js"></script>
     <![endif]-->
 
-    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="shortcut icon" href="/public/favicon.ico">
   </head>
 
   <body>
-    <!-- Part 1: Wrap all page content here -->
     <div id="wrap">
-
-      <!-- Begin page content -->
       <div class="container">
-        <div class="page-header">
+        <header class="page-header">
           <h1>
             PHP caching example
           </h1>
-        </div>
+        </header>
         <p class="lead">For any number N, we'll find the largest prime number 
           less than or equal to N.  Because our implementation is incredibly basic, we 
           don't accept N greater than 10,000.  When you submit the form, we'll check the 
@@ -90,7 +41,7 @@
       </div>
     </div>
 
-    <div id="footer">
+    <footer id="footer">
       <div class="container">
         <p class="muted credit">Example by
           <a href="http://www.memcachier.com">
@@ -100,10 +51,10 @@
           </a>
         </p>
       </div>
-    </div>
+    </footer>
 
-    <script src="/jquery-1.7.2.js"></script>
-    <script src="/main.js"></script>
-    <script src="/bootstrap.js"></script>
+    <script src="/public/jquery-1.7.2.js"></script>
+    <script src="/public/bootstrap.js"></script>
+    <script src="/public/main.js"></script>
   </body>
 </html>
